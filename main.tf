1f6e1cd9c1049b96e7064ecdbde18b30940b629e
Ankit Jain <ankii.jain@gmail.com>
2022-10-21 16:36:34 +0530

    Reference tagged version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ## Security Group for ALB Service/Task
 module "security_group" {
-    source = "git::https://github.com/arjstack/terraform-aws-security-groups.git"
+    source = "git::https://github.com/arjstack/terraform-aws-security-groups.git?ref=v1.0.0"
 
     count = ((local.alb || local.gateway) && var.create_sg) ? 1 : 0
 
